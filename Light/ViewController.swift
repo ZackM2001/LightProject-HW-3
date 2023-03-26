838506116193563117cfbef4a10d4f25d1f56096
Zackary Mazurik <z.mazurik@setonhill.edu>
2023-03-26 02:57:03 -0400

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Zackary Mazurik on 3/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
